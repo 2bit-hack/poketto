cd5f4a745e1c7e026f3cc1aebc3ba756fb2a6aff
2bit-hack <sohamkar76@gmail.com>
2020-08-12 22:03:44 +0530

    Add initial pokeapi fetch and display on flatlist

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,14 +1,64 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  ActivityIndicator,
+  View,
+  FlatList,
+  StyleSheet,
+  Text,
+} from 'react-native';
 import {Button} from 'react-native-paper';
 
+// refactor this into a separate component
+const BasicInfo = ({name}) => {
+  return (
+    <View style={styles.item}>
+      <Text>{name}</Text>
+    </View>
+  );
+};
+
 const MainScreen = ({navigation}) => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  // let displayItem;
+
+  const renderBasicInfo = ({item}) => {
+    return <BasicInfo name={item.name} />;
+  };
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon/?limit=1000')
+      .then((response) => response.json())
+      .then((json) => setData(json.results))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  // if (isLoading) {
+  //   displayItem = <Text>Hello</Text>;
+  // } else {
+  //   displayItem = (
+  //     <FlatList
+  //       data={data.results}
+  //       renderItem={renderBasicInfo}
+  //       keyExtractor={(pokemon) => pokemon.name}
+  //     />
+  //   );
+  // }
+
   return (
     <View style={styles.container}>
-      <Text>poketto</Text>
-      <Button onPress={() => navigation.navigate('Detail')}>
-        Go to details
-      </Button>
+      {/* <Button onPress={() => console.log('Hi')}>Hello</Button> */}
+      {isLoading ? (
+        <ActivityIndicator size="large" />
+      ) : (
+        <FlatList
+          data={data}
+          renderItem={renderBasicInfo}
+          keyExtractor={(pokemon) => pokemon.name}
+        />
+      )}
     </View>
   );
 };
@@ -18,6 +68,12 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
   },
 });
 
